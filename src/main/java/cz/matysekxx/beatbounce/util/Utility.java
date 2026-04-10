2047ecce4b090283ee20679f6a1680149131ad6e
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-11 00:43:07 +0200

    Add utility class

---
+++ src/main/java/cz/matysekxx/beatbounce/util/Utility.java
@@ -0,0 +1,16 @@
+package cz.matysekxx.beatbounce.util;
+
+import java.time.Duration;
+
+public final class Utility {
+    public static void sleep(long millis) {
+        try {Thread.sleep(millis);}
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void sleep(Duration duration) {
+        sleep(duration.toMillis());
+    }
+}

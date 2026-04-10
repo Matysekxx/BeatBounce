d8c76644b8be877ec6d975d9a8349ae8097d7bd8
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-11 01:36:44 +0200

    Add registration of ScreenManager to DIContainer

--- src/main/java/cz/matysekxx/beatbounce/gui/screen/ScreenManager.java
+++ src/main/java/cz/matysekxx/beatbounce/gui/screen/ScreenManager.java
@@ -3,5 +3,9 @@ package cz.matysekxx.beatbounce.gui.screen;
 import cz.matysekxx.beatbounce.DIContainer;
 
 public class ScreenManager {
+    static {
+        DIContainer.register(ScreenManager.class, new ScreenManager());
+    }
 
+    private ScreenManager() {}
 }

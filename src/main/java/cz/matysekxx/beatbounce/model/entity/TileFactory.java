e8fe9ddb9fcc422c5e2e6fa62729d35ab7dd3a73
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-11 23:32:58 +0200

    Add factory methods to TileFactory

--- src/main/java/cz/matysekxx/beatbounce/model/entity/TileFactory.java
+++ src/main/java/cz/matysekxx/beatbounce/model/entity/TileFactory.java
@@ -1,4 +1,18 @@
 package cz.matysekxx.beatbounce.model.entity;
 
+import cz.matysekxx.beatbounce.event.BeatEvent;
+
+import java.awt.Point;
+
 public class TileFactory {
+
+    private TileFactory() {}
+
+    public static NormalTile createNormalTile(BeatEvent event, int x, int y) {
+        return new NormalTile(event, new Point(x, y));
+    }
+
+    public static LongTile createLongTile(BeatEvent event, int x, int y, int widthInPixels) {
+        return new LongTile(event, new Point(x, y), widthInPixels);
+    }
 }

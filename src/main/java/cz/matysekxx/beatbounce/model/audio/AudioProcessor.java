09b3499b7be057d732f8ec35ad0b5c1e0f1ee46b
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-11 23:34:41 +0200

    Improve AudioProcessor

--- src/main/java/cz/matysekxx/beatbounce/model/audio/AudioProcessor.java
+++ src/main/java/cz/matysekxx/beatbounce/model/audio/AudioProcessor.java
@@ -1,38 +1,91 @@
 package cz.matysekxx.beatbounce.model.audio;
 
 import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.io.TarsosDSPAudioFormat;
 import be.tarsos.dsp.onsets.PercussionOnsetDetector;
-import be.tarsos.dsp.io.jvm.AudioDispatcherFactory;
-import cz.matysekxx.beatbounce.model.entity.TileType;
+import cz.matysekxx.beatbounce.event.BeatEvent;
+import cz.matysekxx.beatbounce.event.EventType;
 
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.LineUnavailableException;
 import java.util.function.Consumer;
 
 public class AudioProcessor {
     private final PercussionOnsetDetector detector;
     private final AudioFormat format;
-    public AudioProcessor(AudioFormat format, float speedMultiplier, Consumer<BeatData> onBeatDetected) {
-        this.format = format;
-        this.detector = new PercussionOnsetDetector(format.getSampleRate(), 1024, 8,
-                (time, salience) -> {
-            final double adjustedTime = time / speedMultiplier;
-            final TileType type = TileType.NORMAL;
+    private final Consumer<BeatEvent> onBeatDetected;
+    private final float sampleRate;
+    private double currentTime = 0.0;
 
-            onBeatDetected.accept(new BeatData(adjustedTime, salience, type));
+    private static final double HIGH_INTENSITY_THRESHOLD = 0.15;
+    private static final double LOW_INTENSITY_THRESHOLD = 0.05;
+    private boolean inHighIntensity = false;
+    private boolean inLowIntensity = false;
+
+    public AudioProcessor(AudioFormat format, float speedMultiplier, Consumer<BeatEvent> onBeatDetected) {
+        this.format = format;
+        this.onBeatDetected = onBeatDetected;
+        this.sampleRate = format.getSampleRate();
+
+        this.detector = new PercussionOnsetDetector(sampleRate, 1024, 8,
+                (time, salience) -> {
+            final double adjustedTime = (currentTime + time) / speedMultiplier;
+            onBeatDetected.accept(new BeatEvent(adjustedTime, salience));
         });
     }
 
-    public void processChunk(short[] chunk) throws LineUnavailableException {
-        final float[] floatBuffer = new float[chunk.length];
-        for (int i = 0; i < chunk.length; i++) {
-            floatBuffer[i] = chunk[i] / 32768f;
-        }
+    public void processChunk(short[] chunk) {
+        final float[] floatBuffer = convertToFloatBuffer(chunk);
+        final double rms = calculateRMS(floatBuffer);
 
-        final AudioEvent event = new AudioEvent(AudioDispatcherFactory.fromDefaultMicrophone((int) format.getSampleRate(), 1024).getFormat());
+        final AudioEvent event = createFromFormat(format);
         event.setFloatBuffer(floatBuffer);
+        event.setOverlap(0);
         detector.process(event);
+
+        checkIntensityChanges(rms);
+        currentTime += (double) chunk.length / sampleRate;
     }
 
+    private static AudioEvent createFromFormat(AudioFormat format) {
+        return new AudioEvent(new TarsosDSPAudioFormat(
+                format.getSampleRate(),
+                format.getSampleSizeInBits(),
+                format.getChannels(),
+                true,
+                format.isBigEndian()
+        ));
+    }
 
+    private float[] convertToFloatBuffer(short[] chunk) {
+        final float[] floatBuffer = new float[chunk.length];
+        for (int i = 0; i < chunk.length; i++) floatBuffer[i] = chunk[i] / 32768f;
+
+        return floatBuffer;
+    }
+
+    private double calculateRMS(float[] buffer) {
+        double sumOfSquares = 0.0;
+        for (float sample : buffer) sumOfSquares += sample * sample;
+        return Math.sqrt(sumOfSquares / buffer.length);
+    }
+
+    private void checkIntensityChanges(double rms) {
+        if (rms > HIGH_INTENSITY_THRESHOLD && !inHighIntensity) {
+            onBeatDetected.accept(new BeatEvent(currentTime, EventType.INTENSITY_HIGH_START, rms));
+            inHighIntensity = true;
+            inLowIntensity = false;
+        } else if (rms <= HIGH_INTENSITY_THRESHOLD && inHighIntensity) {
+            onBeatDetected.accept(new BeatEvent(currentTime, EventType.INTENSITY_HIGH_END, rms));
+            inHighIntensity = false;
+        }
+
+        if (rms < LOW_INTENSITY_THRESHOLD && !inLowIntensity) {
+            onBeatDetected.accept(new BeatEvent(currentTime, EventType.INTENSITY_LOW_START, rms));
+            inLowIntensity = true;
+            inHighIntensity = false;
+        } else if (rms >= LOW_INTENSITY_THRESHOLD && inLowIntensity) {
+            onBeatDetected.accept(new BeatEvent(currentTime, EventType.INTENSITY_LOW_END, rms));
+            inLowIntensity = false;
+        }
+    }
 }

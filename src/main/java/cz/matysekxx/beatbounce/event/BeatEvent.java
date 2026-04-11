7a7ebee232e78c054094c30e51814e1c9e37198d
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-11 23:14:19 +0200

    Add BeatEvent record class

---
+++ src/main/java/cz/matysekxx/beatbounce/event/BeatEvent.java
@@ -0,0 +1,15 @@
+package cz.matysekxx.beatbounce.event;
+
+public record BeatEvent(
+    double timestamp,
+    EventType eventType,
+    double salience,
+    double intensityValue
+) {
+    public BeatEvent(double timestamp, double salience) {
+        this(timestamp, EventType.BEAT, salience, 0.0);
+    }
+    public BeatEvent(double timestamp, EventType eventType, double intensityValue) {
+        this(timestamp, eventType, 0.0, intensityValue);
+    }
+}

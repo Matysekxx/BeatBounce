10216378c1e0e9383f27996ff35cf7bce4294f33
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-11 23:32:30 +0200

    Add AudioAnalyzer

---
+++ src/main/java/cz/matysekxx/beatbounce/model/audio/AudioAnalyzer.java
@@ -0,0 +1,42 @@
+package cz.matysekxx.beatbounce.model.audio;
+
+import cz.matysekxx.beatbounce.event.BeatEvent;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class AudioAnalyzer {
+    private final AudioData audioData;
+    private final float speedMultiplier;
+
+    public AudioAnalyzer(AudioData audioData, float speedMultiplier) {
+        this.audioData = audioData;
+        this.speedMultiplier = speedMultiplier;
+    }
+
+    public List<BeatEvent> analyze() {
+        final List<BeatEvent> beatEvents = Collections.synchronizedList(new ArrayList<>());
+
+        final AudioProcessor processor = new AudioProcessor(
+                audioData.format(),
+                speedMultiplier,
+                beatEvents::add
+        );
+
+        final short[] samples = audioData.samples();
+        final int chunkSize = 1024;
+        for (int i = 0; i < samples.length; i += chunkSize) {
+            final int end = Math.min(i + chunkSize, samples.length);
+            final short[] chunk = new short[end - i];
+            System.arraycopy(samples, i, chunk, 0, end - i);
+            processor.processChunk(chunk);
+        }
+
+        beatEvents.sort(Comparator.comparingDouble(BeatEvent::timestamp));
+
+        return beatEvents;
+    }
+}

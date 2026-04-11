7e3e7f6088b00452b9cb144aaa2f130d8cecb1d6
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-11 13:54:59 +0200

    Add Lazy utility class to resolve cycling dependencies

---
+++ src/main/java/cz/matysekxx/beatbounce/util/Lazy.java
@@ -0,0 +1,23 @@
+package cz.matysekxx.beatbounce.util;
+
+import java.util.function.Supplier;
+
+public class Lazy<T> {
+    private final Supplier<T> supplier;
+    private T value;
+
+    public Lazy(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    public static <T> Lazy<T> of(Supplier<T> supplier) {
+        return new Lazy<>(supplier);
+    }
+
+    public T get() {
+        if (value == null) {
+            value = supplier.get();
+        }
+        return value;
+    }
+}

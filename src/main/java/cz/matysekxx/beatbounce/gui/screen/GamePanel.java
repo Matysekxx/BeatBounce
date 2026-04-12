f52c192a6a2e8f6b079806dd389086147bf9b3ea
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-12 15:50:03 +0200

    Add rendering of a track for tiles to GamePanel

--- src/main/java/cz/matysekxx/beatbounce/gui/screen/GamePanel.java
+++ src/main/java/cz/matysekxx/beatbounce/gui/screen/GamePanel.java
@@ -10,7 +10,7 @@ import java.awt.*;
 public class GamePanel extends JPanel implements Runnable {
     private Level level;
     private boolean running;
-    private double cameraX = 0;
+    private double cameraZ = 0;
 
     public GamePanel(Level level) {
         this.level = level;
@@ -31,8 +31,12 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
+        long lastTime = System.currentTimeMillis();
         while (running) {
-            cameraX += 5;
+            long now = System.currentTimeMillis();
+            double deltaTime = (now - lastTime) / 1000.0;
+            lastTime = now;
+            cameraZ += deltaTime * 1000.0;
             repaint();
             Utility.sleep(16);
         }
@@ -43,10 +47,48 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
         final Graphics2D g2d = (Graphics2D) g;
         
-        g2d.translate(-cameraX, 0);
-        for (AbstractTile tile : level.getTiles()) {
-            tile.paint(g2d);
+        final int width = getWidth();
+        final int height = getHeight();
+        final int horizonY = height / 3;
+
+        var xPoints = new int[]{100, width - 100, (width / 2) + 150, (width / 2) - 150};
+        var yPoints = new int[]{height, height, horizonY, horizonY};
+        g2d.setColor(Color.WHITE);
+        g2d.fillPolygon(xPoints, yPoints, 4);
+
+        for (int z = 0; z < 2000; z += 200) {
+            final double distance = z - (cameraZ % 200);
+            if (distance <= 0) continue;
+            final double scale = 250.0 / (distance + 1);
+            final int screenY = horizonY + (int)(150 * scale);
+            if (screenY > horizonY && screenY < height) {
+                g2d.drawLine(0, screenY, width, screenY);
+            }
         }
-        g2d.translate(cameraX, 0);
+
+        g2d.setColor(Color.GREEN);
+
+        final var tiles = level.getTiles();
+        for (int i = tiles.size() - 1; i >= 0; i--) {
+            final AbstractTile tile = tiles.get(i);
+            final double distanceFront = tile.getZ() - cameraZ;
+            if (distanceFront > 0 && distanceFront < 3000) {
+                final double scaleFront = 250.0 / (distanceFront + 1);
+                final int screenYFront = horizonY + (int)(150 * scaleFront);
+
+                final int scaledWidth = (int)(80 * scaleFront);
+                final int screenX = (width / 2) - (scaledWidth / 2);
+
+                final int screenYBack;
+                if (tile.getLengthInZ() > 0) {
+                    final double scaleBack = 250.0 / (distanceFront + tile.getLengthInZ() + 1);
+                    screenYBack = horizonY + (int)(150 * scaleBack);
+                } else {
+                    screenYBack = screenYFront - (int)(20 * scaleFront);
+                }
+                final int scaledHeight = screenYFront - screenYBack;
+                tile.paint3D(g2d, screenX, screenYBack, scaledWidth, scaledHeight);
+            }
+        }
     }
 }

a6a24288fdb595bf97d9672c33fbc99e99c65940
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-12 23:54:15 +0200

    Copy rendering logic of tiles from GamePanel into AbstractTile

--- src/main/java/cz/matysekxx/beatbounce/model/entity/AbstractTile.java
+++ src/main/java/cz/matysekxx/beatbounce/model/entity/AbstractTile.java
@@ -1,7 +1,9 @@
 package cz.matysekxx.beatbounce.model.entity;
 
+import cz.matysekxx.beatbounce.gui.Camera3D;
 import cz.matysekxx.beatbounce.gui.Paintable;
 import cz.matysekxx.beatbounce.event.BeatEvent;
+import cz.matysekxx.beatbounce.gui.WindowData;
 
 import java.awt.*;
 
@@ -27,5 +29,45 @@ public abstract class AbstractTile extends Entity implements Paintable {
         return 0;
     }
 
-    public abstract void paint3D(Graphics2D g2d, Polygon polygon);
+    @Override
+    public void paint3D(Graphics2D g2d, Camera3D cam, WindowData windowData) {
+        final double distance = cam.getDistanceTo(this.getZ());
+        final double length = this.getLengthInZ() > 0 ? this.getLengthInZ() : 50;
+        final double tileDepth = distance + length;
+        final int horizonY = windowData.height() / 3;
+
+        if (tileDepth <= 0 || distance > 3000) return;
+
+        final double scaleFront = cam.getScale(this.getZ());
+        final double scaleBack = cam.getScale(this.getZ() + length);
+
+        final int screenYFront = (int) (horizonY + ((150 - cam.getY()) * scaleFront));
+        final int screenYBack = (int) (horizonY + ((150 - cam.getY()) * scaleBack));
+
+        final double centerScreenFront = calculateCenterScreen(
+                this, (int) cam.getX(), windowData.width(), scaleFront);
+        final double centerScreenBack = calculateCenterScreen(
+                this, (int) cam.getX(), windowData.width(), scaleBack);
+
+        final double frontWidth = 100*scaleFront;
+        final double backWidth = 100*scaleBack;
+
+        final int[] pointsX = {
+                (int) (centerScreenFront - frontWidth / 2),
+                (int) (centerScreenFront + frontWidth / 2),
+                (int) (centerScreenBack + backWidth / 2),
+                (int) (centerScreenBack - backWidth / 2)
+        };
+        final int[] pointsY = {
+                screenYFront, screenYFront, screenYBack, screenYBack
+        };
+        this.paint3D(g2d, new Polygon(
+                pointsX,
+                pointsY,
+                4
+        ));
+    }
+    private double calculateCenterScreen(AbstractTile tile,int camX ,int width ,double scale) {
+        return ((double) width / 2) + ((tile.getX() - camX) * scale);
+    }
 }

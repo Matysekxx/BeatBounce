b6a67d3cba5868755f3b95ba90cd85b14bdd86f0
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-13 00:13:01 +0200

    Simplify paint3D method in AbstractTile

--- src/main/java/cz/matysekxx/beatbounce/model/entity/AbstractTile.java
+++ src/main/java/cz/matysekxx/beatbounce/model/entity/AbstractTile.java
@@ -17,33 +17,36 @@ public abstract class AbstractTile extends Entity implements Paintable {
         this.z = z;
     }
 
-    public BeatEvent getBeatEvent() {
-        return beatEvent;
-    }
-
     public double getZ() {
         return z;
     }
 
     public double getLengthInZ() {
-        return 0;
+        return 50;
     }
 
     @Override
     public void paint3D(Graphics2D g2d, Camera3D cam, WindowData windowData) {
-        final double distance = cam.getDistanceTo(this.getZ());
-        final double length = this.getLengthInZ() > 0 ? this.getLengthInZ() : 50;
-        final double tileDepth = distance + length;
-        final int horizonY = windowData.height() / 3;
-
-        if (tileDepth <= 0 || distance > 3000) return;
-
         final double scaleFront = cam.getScale(this.getZ());
-        final double scaleBack = cam.getScale(this.getZ() + length);
+        final double scaleBack = cam.getScale(this.getZ() + getLengthInZ());
+        this.paint3D(g2d, new Polygon(
+                createXPoints(
+                        cam, windowData, scaleFront, scaleBack),
+                createYPoints(
+                        cam, scaleFront, scaleBack, windowData.height() / 3),
+                4
+        ));
+    }
 
+    private int[] createYPoints(Camera3D cam, double scaleFront, double scaleBack, int horizonY) {
         final int screenYFront = (int) (horizonY + ((150 - cam.getY()) * scaleFront));
         final int screenYBack = (int) (horizonY + ((150 - cam.getY()) * scaleBack));
+        return new int[] {
+                screenYFront, screenYFront, screenYBack, screenYBack
+        };
+    }
 
+    private int[] createXPoints(Camera3D cam, WindowData windowData, double scaleFront, double scaleBack) {
         final double centerScreenFront = calculateCenterScreen(
                 this, (int) cam.getX(), windowData.width(), scaleFront);
         final double centerScreenBack = calculateCenterScreen(
@@ -52,22 +55,14 @@ public abstract class AbstractTile extends Entity implements Paintable {
         final double frontWidth = 100*scaleFront;
         final double backWidth = 100*scaleBack;
 
-        final int[] pointsX = {
+        return new int[]{
                 (int) (centerScreenFront - frontWidth / 2),
                 (int) (centerScreenFront + frontWidth / 2),
                 (int) (centerScreenBack + backWidth / 2),
                 (int) (centerScreenBack - backWidth / 2)
         };
-        final int[] pointsY = {
-                screenYFront, screenYFront, screenYBack, screenYBack
-        };
-        this.paint3D(g2d, new Polygon(
-                pointsX,
-                pointsY,
-                4
-        ));
     }
-    private double calculateCenterScreen(AbstractTile tile,int camX ,int width ,double scale) {
+    private double calculateCenterScreen(AbstractTile tile, int camX, int width, double scale) {
         return ((double) width / 2) + ((tile.getX() - camX) * scale);
     }
 }

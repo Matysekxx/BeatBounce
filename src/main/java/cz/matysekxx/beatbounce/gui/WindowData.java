1fd250f4db6c31d045dddd6d797a7565f45d6acf
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-14 08:45:54 +0200

    Add static factory method to WindowData

--- src/main/java/cz/matysekxx/beatbounce/gui/WindowData.java
+++ src/main/java/cz/matysekxx/beatbounce/gui/WindowData.java
@@ -4,4 +4,7 @@ public record WindowData(
         int width,
         int height
 ) {
+    public static WindowData of(int width, int height) {
+        return new WindowData(width, height);
+    }
 }

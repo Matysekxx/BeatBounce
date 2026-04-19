c51e411970ad53ad21cb489a5df077c664536d5a
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-19 18:16:47 +0200

    Add falling logic to Sphere

--- src/main/java/cz/matysekxx/beatbounce/model/entity/Sphere.java
+++ src/main/java/cz/matysekxx/beatbounce/model/entity/Sphere.java
@@ -11,10 +11,13 @@ public class Sphere extends Entity implements Paintable {
     private double z;
     private double targetX;
     private double currentX;
+    private double currentY;
     private double jumpStartTime;
     private double jumpDuration;
     private double peakHeight;
     private boolean isJumping;
+    private boolean isFalling = false;
+    private double fallSpeed = 0;
 
     public Sphere(int x, int y, int z, int radius) {
         super(x, y);
@@ -22,6 +25,7 @@ public class Sphere extends Entity implements Paintable {
         this.radius = radius;
         this.targetX = x;
         this.currentX = x;
+        this.currentY = y;
         this.isJumping = false;
     }
 
@@ -30,28 +34,56 @@ public class Sphere extends Entity implements Paintable {
         this.jumpDuration = duration;
         this.peakHeight = height;
         this.isJumping = true;
+        this.isFalling = false;
+        this.fallSpeed = 0;
     }
 
     public void update(double currentTime) {
-        currentX += (targetX - currentX) * 0.3;
+        currentX += (targetX - currentX) * 0.7;
         this.x = (int) currentX;
-        if (isJumping) {
+
+        if (isFalling) {
+            fallSpeed += 0.5;
+            currentY += fallSpeed;
+            this.y = (int) currentY;
+        } else if (isJumping) {
             final double elapsed = currentTime - jumpStartTime;
             double progress = elapsed / jumpDuration;
 
             if (progress >= 1.0) {
-                progress = 1.0;
+                isJumping = false;
+                currentY = 150;
+                this.y = 150;
+            } else {
+                final double jumpYOffset = 4 * peakHeight * progress * (1 - progress);
+                currentY = 150 - jumpYOffset;
+                this.y = (int) currentY;
             }
-
-            final double jumpY = 4 * peakHeight * progress * (1 - progress);
-            this.y = (int) (150 - jumpY);
         } else {
+            currentY = 150;
             this.y = 150;
         }
     }
 
     public double getTargetX() {
         return targetX;
+    }
+
+    public double getCurrentY() {
+        return currentY;
+    }
+
+    public void startFalling() {
+        isFalling = true;
+        isJumping = false;
+        fallSpeed = 0;
+    }
+
+    public void reset() {
+        currentX = 0; targetX = 0;
+        currentY = 150; this.y = 150;
+        this.z = 0;
+        isJumping = false; isFalling = false; fallSpeed = 0;
     }
 
     public void setTargetX(double targetX) {
@@ -74,13 +106,17 @@ public class Sphere extends Entity implements Paintable {
         this.z = z;
     }
 
+    public int getRadius() {
+        return radius;
+    }
+
     @Override
     public void paint3D(Graphics2D g2d, Camera3D cam, WindowData windowData) {
         final double scale = cam.getScale(z);
         if (scale <= 0) return;
 
         final int screenX = (int) (windowData.width() / 2. + (currentX - cam.getX()) * scale);
-        final int screenY = (int) (windowData.height() / 3. + (y - cam.getY()) * scale);
+        final int screenY = (int) (windowData.height() / 3. + (currentY - cam.getY()) * scale);
         final int scaledRadius = (int) (radius * scale);
 
         g2d.setColor(Color.MAGENTA);

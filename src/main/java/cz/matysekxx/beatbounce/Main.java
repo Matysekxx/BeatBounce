a887abb27d61869a628d153504e112ed9b33c1c2
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-23 15:32:53 +0200

    Add GPU acceleration for rendering

--- src/main/java/cz/matysekxx/beatbounce/Main.java
+++ src/main/java/cz/matysekxx/beatbounce/Main.java
@@ -3,7 +3,19 @@ package cz.matysekxx.beatbounce;
 import javax.swing.*;
 
 public class Main {
+    static void setProperties() {
+        System.setProperty("sun.java2d.opengl", "true");
+        System.setProperty("sun.java2d.noddraw", "true");
+        System.setProperty("sun.awt.noerasebackground", "true");
+        System.setProperty("sun.java2d.pmoffscreen", "false");
+    }
+
+    static void setTestingProperties() {
+        System.setProperty("sun.java2d.trace", "count");
+    }
     static void main() {
+        setProperties();
+        setTestingProperties();
         SwingUtilities.invokeLater(Execute.getSingleton());
     }
 }

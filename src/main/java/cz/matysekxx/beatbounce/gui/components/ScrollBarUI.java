06b94214b5c2d4949b3cb89839d72950f109437f
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-25 16:41:57 +0200

    Add scrollbarUI

---
+++ src/main/java/cz/matysekxx/beatbounce/gui/components/ScrollBarUI.java
@@ -0,0 +1,28 @@
+package cz.matysekxx.beatbounce.gui.components;
+
+import cz.matysekxx.beatbounce.gui.RenderUtils;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+import java.awt.*;
+
+public class ScrollBarUI extends BasicScrollBarUI {
+    @Override
+    protected void configureScrollBarColors() { 
+        this.thumbColor = new Color(255, 255, 255, 50); 
+        this.trackColor = new Color(0, 0, 0, 0); 
+    }
+    @Override protected JButton createDecreaseButton(int orientation) { return createZeroButton(); }
+    @Override protected JButton createIncreaseButton(int orientation) { return createZeroButton(); }
+    private JButton createZeroButton() { JButton b = new JButton(); b.setPreferredSize(new Dimension(0, 0)); return b; }
+    @Override
+    protected void paintThumb(Graphics g, JComponent c, Rectangle bounds) {
+        if (bounds.isEmpty() || !scrollbar.isEnabled()) return;
+        Graphics2D g2 = (Graphics2D) g.create();
+        RenderUtils.initGraphic2D(g2);
+        g2.setColor(thumbColor);
+        g2.fillRoundRect(bounds.x, bounds.y, bounds.width, bounds.height, bounds.width, bounds.width);
+        g2.dispose();
+    }
+    @Override protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {}
+}

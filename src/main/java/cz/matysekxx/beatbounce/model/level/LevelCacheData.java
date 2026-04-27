d32f44e371619cf19f98ee402c7c3a28f9a55a35
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-27 18:42:03 +0200

    Add LevelCacheData

---
+++ src/main/java/cz/matysekxx/beatbounce/model/level/LevelCacheData.java
@@ -0,0 +1,7 @@
+package cz.matysekxx.beatbounce.model.level;
+
+import cz.matysekxx.beatbounce.model.entity.AbstractTile;
+
+import java.util.List;
+
+public record LevelCacheData(List<AbstractTile> tiles, String songName) {}

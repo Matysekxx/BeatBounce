d54c2251d59af243ca6fe16cb2737f1cd75687ff
Matysekxx <chaloupkamatej1353@gmail.com>
2026-04-27 19:42:34 +0200

    Fix alignment of left side buttons

--- src/main/java/cz/matysekxx/beatbounce/gui/screen/MainMenuScreen.java
+++ src/main/java/cz/matysekxx/beatbounce/gui/screen/MainMenuScreen.java
@@ -32,7 +32,9 @@ public class MainMenuScreen extends Screen {
         backgroundPanel = new MainMenuPanel();
         backgroundPanel.setLayout(new BorderLayout());
         this.setContentPane(backgroundPanel);
+        
         final JPanel sidebar = new SidebarPanel();
+        sidebar.setLayout(new BorderLayout());
 
         final JLabel logo = new JLabel("BeatBounce");
         logo.setFont(new Font("SansSerif", Font.BOLD, 28));
@@ -45,21 +47,24 @@ public class MainMenuScreen extends Screen {
         navLinks.setOpaque(false);
         navLinks.setBorder(new EmptyBorder(0, 15, 0, 15));
 
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+        cardPanel.setOpaque(false);
+
         final Consumer<String> cardSwitcher = target -> cardLayout.show(cardPanel, target);
 
-        navLinks.add(new SidebarButton("Discover", "DISCOVER", true, cardSwitcher));
+        navLinks.add(createAlignedButton(new SidebarButton("Discover", "DISCOVER", true, cardSwitcher)));
         navLinks.add(Box.createRigidArea(new Dimension(0, 5)));
-        // TODO: vytvorit LibaryPanel
-        navLinks.add(new SidebarButton("Library", "LIBRARY", false, null));
+        navLinks.add(createAlignedButton(new SidebarButton("Library", "LIBRARY", false, null)));
         navLinks.add(Box.createRigidArea(new Dimension(0, 5)));
-        // TODO: vytvorit SkinsPanel
-        navLinks.add(new SidebarButton("Skins", "SKINS", false, null));
+        navLinks.add(createAlignedButton(new SidebarButton("Skins", "SKINS", false, null)));
         navLinks.add(Box.createRigidArea(new Dimension(0, 5)));
-        // TODO: vytvorit AchievementsPanel
-        navLinks.add(new SidebarButton("Achievements", "ACHIEVEMENTS", false, null));
+        navLinks.add(createAlignedButton(new SidebarButton("Achievements", "ACHIEVEMENTS", false, null)));
         navLinks.add(Box.createRigidArea(new Dimension(0, 5)));
-        // TODO: vytvorit ShopPanel
-        navLinks.add(new SidebarButton("Shop", "SHOP", false, null));
+        navLinks.add(createAlignedButton(new SidebarButton("Shop", "SHOP", false, null)));
+        
+        navLinks.add(Box.createVerticalGlue());
+        
         sidebar.add(navLinks, BorderLayout.CENTER);
 
         final JPanel bottomSidebar = new JPanel();
@@ -67,24 +72,25 @@ public class MainMenuScreen extends Screen {
         bottomSidebar.setOpaque(false);
         bottomSidebar.setBorder(new EmptyBorder(0, 15, 40, 15));
 
-        final JButton profileBtn = new SidebarButton("Settings", null, false, null);
-        final JButton exitBtn = new SidebarButton("Exit", null, false, null, e -> screenManager.showScreen(IntroScreen.class));
+        final JButton profileBtn = createAlignedButton(new SidebarButton("Settings", null, false, null));
+        final JButton exitBtn = createAlignedButton(new SidebarButton("Exit", null, false, null, e -> screenManager.showScreen(IntroScreen.class)));
         exitBtn.setForeground(new Color(255, 100, 100, 200));
 
         bottomSidebar.add(profileBtn);
         bottomSidebar.add(Box.createRigidArea(new Dimension(0, 5)));
         bottomSidebar.add(exitBtn);
+        
         sidebar.add(bottomSidebar, BorderLayout.SOUTH);
 
         backgroundPanel.add(sidebar, BorderLayout.WEST);
 
-        cardLayout = new CardLayout();
-        cardPanel = new JPanel(cardLayout);
-        cardPanel.setOpaque(false);
-
         cardPanel.add(new DiscoverPanel(audiusClient, objectMapper, screenManager), "DISCOVER");
 
         backgroundPanel.add(cardPanel, BorderLayout.CENTER);
+    }
+    private JButton createAlignedButton(SidebarButton button) {
+        button.setAlignmentX(Component.LEFT_ALIGNMENT);
+        return button;
     }
 
     @Override

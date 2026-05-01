2181b208c9d41c3046cf4ae96b0e0ead99230260
Matysekxx <chaloupkamatej1353@gmail.com>
2026-05-01 21:10:46 +0200

    Improve AudioProcessor

--- src/main/java/cz/matysekxx/beatbounce/model/audio/AudioProcessor.java
+++ src/main/java/cz/matysekxx/beatbounce/model/audio/AudioProcessor.java
@@ -13,48 +13,47 @@ import java.util.function.Consumer;
 public class AudioProcessor {
 
     private static final double HIGH_INTENSITY_THRESHOLD = 0.15;
-
     private static final double LOW_INTENSITY_THRESHOLD = 0.08;
-
     private static final double SMOOTHING_FACTOR = 0.98;
 
     private static final int BUFFER_SIZE = 2048;
     private static final int OVERLAP = 1024;
-    private static final double MIN_BEAT_INTERVAL = 0.20;
+    private static final double MIN_BEAT_INTERVAL = 0.10; 
+
     private final PercussionOnsetDetector percussionDetector;
     private final ComplexOnsetDetector complexDetector;
-    private final AudioFormat format;
+    private final TarsosDSPAudioFormat tarsosFormat;
     private final Consumer<BeatEvent> onBeatDetected;
     private final float sampleRate;
+    
     private double currentTime = 0.0;
     private double smoothedRms = 0.0;
     private boolean inHighIntensity = false;
     private boolean inLowIntensity = false;
     private double lastBeatTime = -1.0;
+    private long samplesProcessed = 0;
 
     public AudioProcessor(AudioFormat format, float speedMultiplier, Consumer<BeatEvent> onBeatDetected) {
-        this.format = format;
-        this.onBeatDetected = onBeatDetected;
-        this.sampleRate = format.getSampleRate();
-        this.percussionDetector = new PercussionOnsetDetector(sampleRate, BUFFER_SIZE,
-                (time, salience) -> handleDetectedBeat(time, salience, speedMultiplier), 85.0, 5.0);
-        this.complexDetector = new ComplexOnsetDetector(BUFFER_SIZE, 0.5);
-        this.complexDetector.setHandler(
-                (time, salience) -> handleDetectedBeat(time, salience, speedMultiplier));
-    }
-
-    private static AudioEvent createFromFormat(AudioFormat format) {
-        return new AudioEvent(new TarsosDSPAudioFormat(
+        this.tarsosFormat = new TarsosDSPAudioFormat(
                 format.getSampleRate(),
                 format.getSampleSizeInBits(),
                 format.getChannels(),
                 true,
                 format.isBigEndian()
-        ));
+        );
+        this.onBeatDetected = onBeatDetected;
+        this.sampleRate = format.getSampleRate();
+
+        this.percussionDetector = new PercussionOnsetDetector(sampleRate, BUFFER_SIZE,
+                (time, salience) -> handleDetectedBeat(time, salience, speedMultiplier), 70.0, 4.0);
+                
+        this.complexDetector = new ComplexOnsetDetector(BUFFER_SIZE, 0.4);
+        this.complexDetector.setHandler(
+                (time, salience) -> handleDetectedBeat(time, salience, speedMultiplier));
     }
 
     private void handleDetectedBeat(double time, double salience, float speedMultiplier) {
-        final double adjustedTime = (currentTime + time) / speedMultiplier;
+        final double adjustedTime = time / speedMultiplier;
         if (lastBeatTime < 0 || (adjustedTime - lastBeatTime) > MIN_BEAT_INTERVAL) {
             onBeatDetected.accept(BeatEvent.of(adjustedTime, salience));
             lastBeatTime = adjustedTime;
@@ -65,22 +64,26 @@ public class AudioProcessor {
         final float[] floatBuffer = convertToFloatBuffer(chunk);
         final double rms = calculateRMS(floatBuffer);
 
-        final AudioEvent event = createFromFormat(format);
+        final AudioEvent event = new AudioEvent(tarsosFormat);
         event.setFloatBuffer(floatBuffer);
         event.setOverlap(OVERLAP);
+
+        final long bytesProcessed = samplesProcessed * tarsosFormat.getFrameSize();
+        event.setBytesProcessed(bytesProcessed);
 
         percussionDetector.process(event);
         complexDetector.process(event);
 
         checkIntensityChanges(rms);
-        currentTime += (double) (chunk.length - OVERLAP) / sampleRate;
-        if (currentTime < 0) currentTime = 0;
+
+        final int stepSize = chunk.length - OVERLAP;
+        samplesProcessed += stepSize;
+        currentTime = (double) samplesProcessed / sampleRate;
     }
 
     private float[] convertToFloatBuffer(short[] chunk) {
         final float[] floatBuffer = new float[chunk.length];
         for (int i = 0; i < chunk.length; i++) floatBuffer[i] = chunk[i] / 32768f;
-
         return floatBuffer;
     }
 
@@ -92,7 +95,6 @@ public class AudioProcessor {
 
     private void checkIntensityChanges(double rms) {
         smoothedRms = (smoothedRms * SMOOTHING_FACTOR) + (rms * (1.0 - SMOOTHING_FACTOR));
-
         if (smoothedRms > HIGH_INTENSITY_THRESHOLD && !inHighIntensity) {
             onBeatDetected.accept(BeatEvent.of(currentTime, EventType.INTENSITY_HIGH_START, smoothedRms));
             inHighIntensity = true;

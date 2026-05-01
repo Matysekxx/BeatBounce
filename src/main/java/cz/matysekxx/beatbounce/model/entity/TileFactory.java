637e0e2863db8d520448f6772fc5e781b25c02eb
Matysekxx <chaloupkamatej1353@gmail.com>
2026-05-01 21:14:14 +0200

    Add factory method to create fake tile in TileFactory

--- src/main/java/cz/matysekxx/beatbounce/model/entity/TileFactory.java
+++ src/main/java/cz/matysekxx/beatbounce/model/entity/TileFactory.java
@@ -12,12 +12,16 @@ public class TileFactory {
     public static NormalTile createNormalTile(BeatEvent event, int x, int y, double z) {
         return new NormalTile(event, new Point(x, y), z);
     }
+    
+    public static NormalTile createFakeNormalTile(BeatEvent event, int x, int y, double z, int fakeDirection) {
+        return new NormalTile(event, new Point(x, y), z, fakeDirection);
+    }
 
     public static LongTile createLongTile(BeatEvent event, int x, int y, double z, double lengthInZ) {
         return new LongTile(event, new Point(x, y), z, lengthInZ);
     }
 
     public static MovingTile createMovingTile(BeatEvent event, int x, int y, double z, int amplitude, double speed) {
-        return new MovingTile(event, new Point(x, y), z, amplitude, speed);
+        return new MovingTile(event, x, y, z, amplitude, speed);
     }
 }

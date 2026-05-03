30988e0e9247885df09ebf12dcc8b8e2b80c3e80
Matysekxx <chaloupkamatej1353@gmail.com>
2026-05-03 17:49:03 +0200

    Implement system properties opengl, useSystemAAFontSettings, noddraw, aatext to main

--- src/main/java/cz/matysekxx/beatbounce/Main.java
+++ src/main/java/cz/matysekxx/beatbounce/Main.java
@@ -1,18 +1,21 @@
 package cz.matysekxx.beatbounce;
 
+import cz.matysekxx.beatbounce.configuration.Settings;
+
 import javax.swing.*;
 
 public class Main {
     static void setProperties() {
-        System.setProperty("sun.java2d.opengl", "true");
-        System.setProperty("sun.java2d.noddraw", "true");
+        System.setProperty("sun.java2d.opengl", Settings.opengl ? "true" : "false");
+        System.setProperty("sun.java2d.noddraw", Settings.opengl ? "true" : "false");
         System.setProperty("sun.awt.noerasebackground", "true");
         System.setProperty("sun.java2d.pmoffscreen", "false");
-        System.setProperty("awt.useSystemAAFontSettings", "on");
-        System.setProperty("swing.aatext", "true");
+        System.setProperty("awt.useSystemAAFontSettings", Settings.graphicsQuality.equals("LOW") ? "off" : "on");
+        System.setProperty("swing.aatext", Settings.graphicsQuality.equals("LOW") ? "false" : "true");
     }
 
     static void main() {
+        Settings.load();
         setProperties();
         SwingUtilities.invokeLater(Execute.getSingleton());
     }

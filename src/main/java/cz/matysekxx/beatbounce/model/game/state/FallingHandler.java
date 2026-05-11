5b8100ddc92188c61d157d1e8d2f92fbf8528b16
Matysekxx <chaloupkamatej1353@gmail.com>
2026-05-11 20:42:05 +0200

    Add FallingHandler

---
+++ src/main/java/cz/matysekxx/beatbounce/model/game/state/FallingHandler.java
@@ -0,0 +1,26 @@
+package cz.matysekxx.beatbounce.model.game.state;
+
+import cz.matysekxx.beatbounce.model.entity.Sphere;
+import cz.matysekxx.beatbounce.model.game.GameEngine;
+import cz.matysekxx.beatbounce.model.score.ScoreManager;
+
+public class FallingHandler implements GameStateHandler {
+    private final GameEngine gameEngine;
+    private final Sphere sphere;
+
+    public FallingHandler(GameEngine gameEngine, Sphere sphere) {
+        this.gameEngine = gameEngine;
+        this.sphere = sphere;
+    }
+
+    @Override
+    public void handle(double currentTime, double deltaTime) {
+        sphere.update(currentTime, deltaTime);
+        sphere.setZ(gameEngine.getFallStartZ());
+        gameEngine.getCam().setZ(gameEngine.getGameZProgress() - 500);
+        if (sphere.getCurrentY() > 500) {
+            gameEngine.setGameState(GameState.GAME_OVER);
+            ScoreManager.updateScore(gameEngine.getCleanSongName(), gameEngine.getScore());
+        }
+    }
+}

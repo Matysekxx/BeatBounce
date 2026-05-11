91214cb32d8ae723ed3671deafd0f2faedd97641
Matysekxx <chaloupkamatej1353@gmail.com>
2026-05-11 21:35:12 +0200

    Add SmallCollisionHandler

---
+++ src/main/java/cz/matysekxx/beatbounce/model/game/collision/SmallCollisionHandler.java
@@ -0,0 +1,16 @@
+package cz.matysekxx.beatbounce.model.game.collision;
+
+import cz.matysekxx.beatbounce.model.entity.AbstractTile;
+import cz.matysekxx.beatbounce.model.game.GameEngine;
+
+public class SmallCollisionHandler extends CollisionHandler{
+    protected SmallCollisionHandler(GameEngine gameEngine) {
+        super(gameEngine);
+    }
+
+    @Override
+    public void handle(AbstractTile tile) {
+        final int scoreIncrease = 15;
+        advanceTile(scoreIncrease);
+    }
+}

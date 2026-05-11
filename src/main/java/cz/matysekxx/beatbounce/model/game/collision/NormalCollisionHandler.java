318ceeb05ac6f2c9263214e78619c9056715c8ae
Matysekxx <chaloupkamatej1353@gmail.com>
2026-05-11 21:35:27 +0200

    Add NormalCollisionHandler

---
+++ src/main/java/cz/matysekxx/beatbounce/model/game/collision/NormalCollisionHandler.java
@@ -0,0 +1,16 @@
+package cz.matysekxx.beatbounce.model.game.collision;
+
+import cz.matysekxx.beatbounce.model.entity.AbstractTile;
+import cz.matysekxx.beatbounce.model.game.GameEngine;
+
+public class NormalCollisionHandler extends CollisionHandler{
+    protected NormalCollisionHandler(GameEngine gameEngine) {
+        super(gameEngine);
+    }
+
+    @Override
+    public void handle(AbstractTile tile) {
+        final int scoreIncrease = 10;
+        advanceTile(scoreIncrease);
+    }
+}

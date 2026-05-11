12541b3d5820f996bb58020d96387e6d4ac68fe2
Matysekxx <chaloupkamatej1353@gmail.com>
2026-05-11 21:34:58 +0200

    Add SpeedCollisionHandler

---
+++ src/main/java/cz/matysekxx/beatbounce/model/game/collision/SpeedCollisionHandler.java
@@ -0,0 +1,25 @@
+package cz.matysekxx.beatbounce.model.game.collision;
+
+import cz.matysekxx.beatbounce.model.entity.AbstractTile;
+import cz.matysekxx.beatbounce.model.entity.SpeedTile;
+import cz.matysekxx.beatbounce.model.game.GameEngine;
+
+public class SpeedCollisionHandler extends  CollisionHandler {
+    private static final double SPEED_EFFECT_DURATION = 3.0;
+    protected SpeedCollisionHandler(GameEngine gameEngine) {
+        super(gameEngine);
+    }
+
+    @Override
+    public void handle(AbstractTile tile) {
+        if (tile instanceof SpeedTile st) {
+            advanceTile(10);
+            if (!st.isActivated()) {
+                st.activate();
+                gameEngine.setSpeedEffectActive(true);
+                gameEngine.setSpeedEffectTimeRemaining(SPEED_EFFECT_DURATION);
+                gameEngine.setActiveSpeedMultiplier(st.getSpeedMultiplier());
+            }
+        }
+    }
+}

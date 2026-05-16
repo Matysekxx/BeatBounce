86cb579a4223175463ebc2f4c22c2fcafee720fd
Matysekxx <chaloupkamatej1353@gmail.com>
2026-05-16 23:47:47 +0200

    Fix LongCollisionHandler

--- src/main/java/cz/matysekxx/beatbounce/model/game/collision/LongCollisionHandler.java
+++ src/main/java/cz/matysekxx/beatbounce/model/game/collision/LongCollisionHandler.java
@@ -1,5 +1,6 @@
 package cz.matysekxx.beatbounce.model.game.collision;
 
+import cz.matysekxx.beatbounce.gui.components.ScorePopup;
 import cz.matysekxx.beatbounce.model.entity.AbstractTile;
 import cz.matysekxx.beatbounce.model.entity.LongTile;
 import cz.matysekxx.beatbounce.model.game.GameEngine;
@@ -36,10 +37,19 @@ public class LongCollisionHandler extends CollisionHandler {
         final double timeToNextTile = (nextTile.getZ() - gameEngine.getGameZProgress()) / gameEngine.getzUnitsPerSecond();
         final boolean shouldJumpEarly = timeToNextTile <= 0.25;
 
+        final double sphereX = gameEngine.getSphere().getX();
+        final double tileX = lt.getX();
+        if (Math.abs(sphereX - tileX) > 60) {
+            this.onLongTile = false;
+            gameEngine.startFalling();
+            return false;
+        }
+
         if (gameEngine.getGameZProgress() <= lt.getZ() + lt.getLengthInZ() && !shouldJumpEarly) {
             gameEngine.setLongTileScoreAccum(gameEngine.getLongTileScoreAccum() + 1);
             if (gameEngine.getLongTileScoreAccum() % 6 == 0) {
                 gameEngine.setScore(gameEngine.getScore() + 1);
+                gameEngine.addScorePopup(ScorePopup.createRandom(1, 0, 120));
             }
             return true;
         } else {

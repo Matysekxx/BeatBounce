b7c150072c1ff2fffcf55cd28cf542b87602ff87
Matysek <chaloupkamatej1353@gmail.com>
2026-05-28 22:36:33 +0200

    Add documentation to HighScoreEvaluator.java

--- src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/HighScoreEvaluator.java
+++ src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/HighScoreEvaluator.java
@@ -3,7 +3,17 @@ package cz.matysekxx.beatbounce.model.achievement.evaluator;
 import cz.matysekxx.beatbounce.model.achievement.AchievementSaveData;
 import cz.matysekxx.beatbounce.model.score.ScoreManager;
 
+/**
+ * Evaluator for the high score achievement.
+ * Returns the global high score from the ScoreManager.
+ */
 public class HighScoreEvaluator implements AchievementEvaluator {
+    /**
+     * Evaluates the current high score.
+     *
+     * @param saveData The achievement save data (unused in this evaluator).
+     * @return The current global high score.
+     */
     @Override
     public int evaluate(AchievementSaveData saveData) {
         return ScoreManager.getGlobalHighScore();

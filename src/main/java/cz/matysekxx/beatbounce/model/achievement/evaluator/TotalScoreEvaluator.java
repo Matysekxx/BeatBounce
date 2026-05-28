1c414210c2f999d07d62131deae05c73a5bf443f
Matysek <chaloupkamatej1353@gmail.com>
2026-05-28 22:39:09 +0200

    Add documentation to TotalScoreEvaluator.java

--- src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/TotalScoreEvaluator.java
+++ src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/TotalScoreEvaluator.java
@@ -3,7 +3,17 @@ package cz.matysekxx.beatbounce.model.achievement.evaluator;
 import cz.matysekxx.beatbounce.model.achievement.AchievementSaveData;
 import cz.matysekxx.beatbounce.model.score.ScoreManager;
 
+/**
+ * Evaluator for achievements based on the cumulative score across all plays.
+ * It retrieves the total score from the global ScoreManager.
+ */
 public class TotalScoreEvaluator implements AchievementEvaluator {
+    /**
+     * Returns the total cumulative score achieved by the player.
+     *
+     * @param saveData the achievement save data (unused)
+     * @return the total global score
+     */
     @Override
     public int evaluate(AchievementSaveData saveData) {
         return ScoreManager.getTotalScore();

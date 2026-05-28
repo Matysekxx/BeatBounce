0075d3bd2d77e38677bcb599b3b5c47a7391ef82
Matysek <chaloupkamatej1353@gmail.com>
2026-05-28 22:32:39 +0200

    Add documentation to AchievementEvaluator.java

--- src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/AchievementEvaluator.java
+++ src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/AchievementEvaluator.java
@@ -2,6 +2,17 @@ package cz.matysekxx.beatbounce.model.achievement.evaluator;
 
 import cz.matysekxx.beatbounce.model.achievement.AchievementSaveData;
 
+/**
+ * Interface for achievement progress evaluators.
+ * Each implementation defines how to calculate the current progress value
+ * for a specific achievement type.
+ */
 public interface AchievementEvaluator {
+    /**
+     * Evaluates the current progress for an achievement.
+     *
+     * @param saveData the persistent achievement save data
+     * @return the calculated progress value as an integer
+     */
     int evaluate(AchievementSaveData saveData);
 }

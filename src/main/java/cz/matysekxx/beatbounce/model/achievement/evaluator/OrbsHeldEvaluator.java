bf8c9202e16c21921773c3f0999166eeb24597aa
Matysek <chaloupkamatej1353@gmail.com>
2026-05-28 22:37:28 +0200

    Add documentation to OrbsHeldEvaluator.java

--- src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/OrbsHeldEvaluator.java
+++ src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/OrbsHeldEvaluator.java
@@ -3,7 +3,17 @@ package cz.matysekxx.beatbounce.model.achievement.evaluator;
 import cz.matysekxx.beatbounce.model.achievement.AchievementSaveData;
 import cz.matysekxx.beatbounce.model.score.ScoreManager;
 
+/**
+ * Evaluator for the orbs held achievement.
+ * Returns the current currency (orbs) from the ScoreManager.
+ */
 public class OrbsHeldEvaluator implements AchievementEvaluator {
+    /**
+     * Evaluates the current number of orbs held.
+     *
+     * @param saveData The achievement save data (unused in this evaluator).
+     * @return The current amount of currency.
+     */
     @Override
     public int evaluate(AchievementSaveData saveData) {
         return ScoreManager.getCurrency();

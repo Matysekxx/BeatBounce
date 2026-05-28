d1f3d9ba44180b7a9e53db55c963e21b4d518465
Matysek <chaloupkamatej1353@gmail.com>
2026-05-28 22:39:35 +0200

    Add documentation to UniqueSongsEvaluator.java

--- src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/UniqueSongsEvaluator.java
+++ src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/UniqueSongsEvaluator.java
@@ -3,7 +3,17 @@ package cz.matysekxx.beatbounce.model.achievement.evaluator;
 import cz.matysekxx.beatbounce.model.achievement.AchievementSaveData;
 import cz.matysekxx.beatbounce.model.score.ScoreManager;
 
+/**
+ * Evaluator for achievements based on the number of unique songs played.
+ * It tracks how many different tracks the player has attempted.
+ */
 public class UniqueSongsEvaluator implements AchievementEvaluator {
+    /**
+     * Returns the count of unique songs that have been played.
+     *
+     * @param saveData the achievement save data (unused)
+     * @return the number of unique songs played
+     */
     @Override
     public int evaluate(AchievementSaveData saveData) {
         return ScoreManager.getSongsPlayedCount();

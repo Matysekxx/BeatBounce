0b85ce361c35d9349684a313d5367a401bc9348b
Matysek <chaloupkamatej1353@gmail.com>
2026-05-28 22:39:01 +0200

    Add documentation to TotalPlaysEvaluator.java

--- src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/TotalPlaysEvaluator.java
+++ src/main/java/cz/matysekxx/beatbounce/model/achievement/evaluator/TotalPlaysEvaluator.java
@@ -2,7 +2,17 @@ package cz.matysekxx.beatbounce.model.achievement.evaluator;
 
 import cz.matysekxx.beatbounce.model.achievement.AchievementSaveData;
 
+/**
+ * Evaluator for achievements based on the total number of games played.
+ * It retrieves the play count directly from the achievement save data.
+ */
 public class TotalPlaysEvaluator implements AchievementEvaluator {
+    /**
+     * Returns the total number of plays recorded in the save data.
+     *
+     * @param saveData the persistent achievement save data
+     * @return the total number of games played
+     */
     @Override
     public int evaluate(AchievementSaveData saveData) {
         return saveData.getTotalPlays();
